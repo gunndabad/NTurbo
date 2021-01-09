71c34448e46f4076a348ef6eebaa04a084bb7556
James Gunn <james@gunn.io>
2021-01-09 12:55:11 +0000

    Add a placeholder method for adding services to IServiceCollection

---
+++ server/src/NTurbo/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using NTurbo.SignalR;
+
+namespace NTurbo
+{
+    /// <summary>
+    /// Extension methods for setting up NTurbo services in an <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds NTurbo services to the specified <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddNTurbo(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            return services;
+        }
+    }
+}
